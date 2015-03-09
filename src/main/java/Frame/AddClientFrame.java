7e3e10e0e5ad27a97a671f5d228f6c62d0469d90
Den-K <denis.kobernik@gmail.com>
2015-03-09 23:10:55 +0200

    AddClientFrame added

---
+++ src/main/java/Frame/AddClientFrame.java
@@ -0,0 +1,14 @@
+package Frame;
+
+import javax.swing.*;
+
+/**
+ * Created by Den on 09.03.2015.
+ */
+public class AddClientFrame  extends JFrame{
+
+    public AddClientFrame(){
+        super("Add client");
+        setVisible(true);
+    }
+}
